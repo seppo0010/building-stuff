e20a2363cb71a7ae6a4f114d6ae2056804c355bd
Sebastian Waisbrot <seppo0010@gmail.com>
2019-01-12 17:52:17 -0300

    Catch up when lagging behind

--- src/systems/physics.rs
+++ src/systems/physics.rs
@@ -1,21 +1,34 @@
 use crate::{components::PhysicsBody, resources::MyWorld};
 
 use amethyst::{
-    core::Transform,
-    ecs::{Join, ReadStorage, System, Write, WriteStorage},
+    core::{timing::Time, Transform},
+    ecs::{Join, Read, ReadStorage, System, Write, WriteStorage},
 };
 
+const MAX_STEPS_PER_RUN: u8 = 4;
+
 #[derive(Default)]
-pub struct PhysicsSystem;
+pub struct PhysicsSystem {
+    time_accumulator: f32,
+}
 
 impl<'s> System<'s> for PhysicsSystem {
     type SystemData = (
         Write<'s, MyWorld>,
         WriteStorage<'s, Transform>,
         ReadStorage<'s, PhysicsBody>,
+        Read<'s, Time>,
     );
-    fn run(&mut self, (mut physics_world, mut transforms, bodies): Self::SystemData) {
-        physics_world.step();
+    fn run(&mut self, (mut physics_world, mut transforms, bodies, time): Self::SystemData) {
+        self.time_accumulator += time.delta_seconds();
+        let timestep = physics_world.timestep();
+        for _ in 0..MAX_STEPS_PER_RUN {
+            physics_world.step();
+            self.time_accumulator -= timestep;
+            if self.time_accumulator < timestep {
+                break;
+            }
+        }
         for (mut t, body) in (&mut transforms, &bodies).join() {
             if let Some(pos) = physics_world
                 .collider_body_handle(body.0)
