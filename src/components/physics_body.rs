305cb80327c6becc6d95efd6066ccf5538578e6e
Sebastian Waisbrot <sebastian.waisbrot@despegar.com>
2020-02-25 20:58:12 -0300

    Made rustc panic

--- src/components/physics_body.rs
+++ src/components/physics_body.rs
@@ -1,7 +1,7 @@
 use amethyst::ecs::{Component, VecStorage};
-use ncollide3d::world::CollisionObjectHandle;
+use ncollide3d::world::CollisionObjectSlabHandle;
 
-pub struct PhysicsBody(pub CollisionObjectHandle);
+pub struct PhysicsBody(pub CollisionObjectSlabHandle);
 impl Component for PhysicsBody {
     type Storage = VecStorage<Self>;
 }
